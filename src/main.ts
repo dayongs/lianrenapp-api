1245c68bc30d90068c827735555de79db0054636
mengfei0619 <mengfei0619@gmail.com>
2021-10-26 14:33:41 +0800

    初始化

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {join} from "path";
+import {NestExpressApplication} from "@nestjs/platform-express";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  //创建实例
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   // app.setGlobalPrefix('api'); // 全局路由前缀 例如  localhost:3000/api
-  await app.listen(3000);
+
+  // 处理跨域
+  app.enableCors();
+
+  // 在根目录创建静态文件 'public'
+  // app.useStaticAssets('public');
+
+  //设置虚拟路径
+  app.useStaticAssets(join(__dirname, '..', 'public'),{
+    prefix: '/static/',   //目录地址
+  });
+
+  await app.listen(4000);
 }
 bootstrap();
